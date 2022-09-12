32123ea72717496e8d8d6c2ed88c6c2066462d59
Javier Duarte <jduarte@ucsd.edu>
2022-09-12 03:31:36 +0000

    try spanet; add baseline

---
+++ src/data/convert_to_h5.py
@@ -0,0 +1,117 @@
+import logging
+import os.path as osp
+
+import awkward as ak
+import numpy as np
+import uproot
+import vector
+from coffea.nanoevents import BaseSchema, NanoEventsFactory
+import h5py
+import click
+
+vector.register_awkward()
+
+logging.basicConfig(level=logging.INFO)
+
+N_JETS = 10
+MIN_JET_PT = 20
+MIN_JETS = 6
+N_HIGGS = 3
+FEATURE_BRANCHES = ["jet{i}Pt", "jet{i}Eta", "jet{i}Phi", "jet{i}DeepFlavB", "jet{i}JetId"]
+LABEL_BRANCHES = ["jet{i}HiggsMatchedIndex", "jet{i}HadronFlavour"]
+ALL_BRANCHES = [branch.format(i=i) for i in range(1, N_JETS + 1) for branch in FEATURE_BRANCHES + LABEL_BRANCHES]
+RAW_FILE_NAME = "GluGluToHHHTo6B_SM.root"
+
+
+def get_n_features(name, events, n):
+    return ak.concatenate([np.expand_dims(events[name.format(i=i)], axis=-1) for i in range(1, n + 1)], axis=-1)
+
+
+@click.command()
+@click.option('--out-file', default = "hhh_training.h5", help="Output file.")
+@click.option('--train-frac', default=0.95, help="Fraction for training.")
+def main(out_file, train_frac):
+    in_file = uproot.open(osp.join("data", RAW_FILE_NAME))
+    num_entries = in_file["Events"].num_entries
+    if "training" in out_file:
+        entry_start = None
+        entry_stop = int(train_frac*num_entries)
+    else:
+        entry_start = int(train_frac*num_entries)
+        entry_stop = None
+    events = NanoEventsFactory.from_root(
+        in_file, treepath="Events", entry_start=entry_start, entry_stop=entry_stop, schemaclass=BaseSchema
+    ).events()
+
+    pt = get_n_features("jet{i}Pt", events, N_JETS)
+    eta = get_n_features("jet{i}Eta", events, N_JETS)
+    phi = get_n_features("jet{i}Phi", events, N_JETS)
+    btag = get_n_features("jet{i}DeepFlavB", events, N_JETS)
+    jet_id = get_n_features("jet{i}JetId", events, N_JETS)
+    higgs_idx = get_n_features("jet{i}HiggsMatchedIndex", events, N_JETS)
+    hadron_flavor = get_n_features("jet{i}HadronFlavour", events, N_JETS)
+
+    # keep events with MIN_JETS jets
+    mask = ak.num(pt[pt > MIN_JET_PT]) >= MIN_JETS
+    pt = pt[mask]
+    eta = eta[mask]
+    phi = phi[mask]
+    btag = btag[mask]
+    jet_id = jet_id[mask]
+    higgs_idx = higgs_idx[mask]
+    hadron_flavor = hadron_flavor[mask]
+
+    # mask to define zero-padded jets
+    mask = pt > MIN_JET_PT
+
+    # require hadron_flavor == 5 (i.e. b-jet ghost association matching)
+    higgs_idx = ak.where(higgs_idx != 0, ak.where(hadron_flavor == 5, higgs_idx, -1), 0)
+
+    h1_bs = ak.local_index(higgs_idx)[higgs_idx == 1]
+    h2_bs = ak.local_index(higgs_idx)[higgs_idx == 2]
+    h3_bs = ak.local_index(higgs_idx)[higgs_idx == 3]
+
+    check = np.unique(ak.count(h1_bs, axis=-1))
+    if 3 in check.to_list():
+        logging.warning("some 1st Higgs bosons match to 3 jets! Check truth")
+    check = np.unique(ak.count(h2_bs, axis=-1))
+    if 3 in check.to_list():
+        logging.warning("some 2nd Higgs bosons match to 3 jets! Check truth")
+    check = np.unique(ak.count(h3_bs, axis=-1))
+    if 3 in check.to_list():
+        logging.warning("some 3rd Higgs bosons match to 3 jets! Check truth")
+
+    h1_bs = ak.fill_none(ak.pad_none(h1_bs, 2, clip=True), -1)
+    h2_bs = ak.fill_none(ak.pad_none(h2_bs, 2, clip=True), -1)
+    h3_bs = ak.fill_none(ak.pad_none(h3_bs, 2, clip=True), -1)
+
+    h1_b1, h1_b2 = h1_bs[:,0], h1_bs[:,1]
+    h2_b1, h2_b2 = h2_bs[:,0], h2_bs[:,1]
+    h3_b1, h3_b2 = h3_bs[:,0], h3_bs[:,1]
+
+    h1_mask = ak.all(h1_bs != -1, axis=-1)
+    h2_mask = ak.all(h2_bs != -1, axis=-1)
+    h3_mask = ak.all(h3_bs != -1, axis=-1)
+
+    with h5py.File(osp.join("data", out_file), 'w') as output:
+        output.create_dataset(f"source/mask", data=mask.to_numpy())
+        output.create_dataset(f"source/btag", data=btag.to_numpy())
+        output.create_dataset(f"source/pt", data=pt.to_numpy())
+        output.create_dataset(f"source/eta", data=eta.to_numpy())
+        output.create_dataset(f"source/phi", data=phi.to_numpy())
+        output.create_dataset(f"source/jetid", data=jet_id.to_numpy())
+
+        output.create_dataset(f"h1/mask", data=h1_mask.to_numpy())
+        output.create_dataset(f"h1/b1", data=h1_b1.to_numpy())
+        output.create_dataset(f"h1/b2", data=h1_b2.to_numpy())
+
+        output.create_dataset(f"h2/mask", data=h2_mask.to_numpy())
+        output.create_dataset(f"h2/b1", data=h2_b1.to_numpy())
+        output.create_dataset(f"h2/b2", data=h2_b2.to_numpy())
+        
+        output.create_dataset(f"h3/mask", data=h3_mask.to_numpy())
+        output.create_dataset(f"h3/b1", data=h3_b1.to_numpy())
+        output.create_dataset(f"h3/b2", data=h3_b2.to_numpy())
+
+if __name__ == "__main__":
+    main()
---
+++ src/models/test_baseline.py
@@ -0,0 +1,112 @@
+import itertools
+import logging
+import os.path as osp
+
+import awkward as ak
+import numpy as np
+import torch
+import uproot
+import vector
+import itertools
+from coffea.nanoevents import BaseSchema, NanoEventsFactory
+import click
+
+from src.data.convert_to_h5 import MIN_JET_PT, MIN_JETS, N_JETS, get_n_features, RAW_FILE_NAME
+
+vector.register_awkward()
+
+logging.basicConfig(level=logging.INFO)
+
+HIGGS_MASS = 125.0
+# precompute possible jet assignments lookup table
+JET_ASSIGNMENTS = {}
+for nj in range(MIN_JETS, N_JETS+1):
+    a = list(itertools.combinations(range(nj), 2))
+    b = np.array([(i, j, k) for i, j, k in itertools.combinations(a, 3) if len(set(i + j + k)) == MIN_JETS])
+    JET_ASSIGNMENTS[nj] = b
+
+@click.command()
+@click.option('--test-frac', default=0.05, help="Fraction for testing.")
+def main(test_frac):
+    in_file = uproot.open(osp.join("data", RAW_FILE_NAME))
+    num_entries = in_file["Events"].num_entries
+    entry_start = int((1-test_frac)*num_entries)
+    entry_stop = None
+    test_root = osp.join(osp.dirname(osp.realpath(__file__)), "..", "..", "data")
+    events = NanoEventsFactory.from_root(
+        in_file,
+        treepath="Events",
+        entry_start=entry_start,
+        entry_stop=entry_stop,
+        schemaclass=BaseSchema,
+    ).events()
+
+    pt = get_n_features("jet{i}Pt", events, N_JETS)
+    eta = get_n_features("jet{i}Eta", events, N_JETS)
+    phi = get_n_features("jet{i}Phi", events, N_JETS)
+    btag = get_n_features("jet{i}DeepFlavB", events, N_JETS)
+    jet_id = get_n_features("jet{i}JetId", events, N_JETS)
+    higgs_idx = get_n_features("jet{i}HiggsMatchedIndex", events, N_JETS)
+
+    # remove jets below MIN_JET_PT (i.e. zero-padded jets)
+    mask = pt > MIN_JET_PT
+    pt = pt[mask]
+    eta = eta[mask]
+    phi = phi[mask]
+    btag = btag[mask]
+    jet_id = jet_id[mask]
+    higgs_idx = higgs_idx[mask]
+
+    # keep events with MIN_JETS jets
+    mask = ak.num(pt) >= MIN_JETS
+    pt = pt[mask]
+    eta = eta[mask]
+    phi = phi[mask]
+    btag = btag[mask]
+    jet_id = jet_id[mask]
+    higgs_idx = higgs_idx[mask]
+
+    jets = ak.zip(
+        {
+            "pt": pt,
+            "eta": eta,
+            "phi": phi,
+            "mass": ak.zeros_like(pt),
+            "btag": btag,
+            "jet_id": jet_id,
+            "higgs_idx": higgs_idx,
+        },
+        with_name="Momentum4D",
+    )
+
+    n_events = len(jets.pt)
+    higgs_1_reco = np.zeros((n_events), dtype=bool)
+    higgs_2_reco = np.zeros((n_events), dtype=bool)
+    higgs_3_reco = np.zeros((n_events), dtype=bool)
+
+    for i in range(n_events):
+        # nj = len(jets.pt[i])
+        nj = 6
+        mjj = (jets[i, JET_ASSIGNMENTS[nj][:, :, 0]] + jets[i, JET_ASSIGNMENTS[nj][:, :, 1]]).mass
+        chi2 = ak.sum(np.square(mjj - HIGGS_MASS), axis=-1)
+        chi2_argmin = ak.argmin(chi2, axis=-1)
+        truth = jets[i][JET_ASSIGNMENTS[nj][chi2_argmin]].higgs_idx.to_list()
+        higgs_1_reco[i] = ([1, 1] in truth)
+        higgs_2_reco[i] = ([2, 2] in truth)
+        higgs_3_reco[i] = ([3, 3] in truth)
+
+
+    higgs_1_frac = np.sum(higgs_1_reco)/n_events
+    higgs_2_frac = np.sum(higgs_2_reco)/n_events
+    higgs_3_frac = np.sum(higgs_3_reco)/n_events
+    all_higgs_frac = np.sum(higgs_1_reco*higgs_2_reco*higgs_3_reco)/n_events
+
+    logging.info("Method: 125 GeV")
+    loggig.info("Higgs 1 fraction: {higgs_1_frac:.4f}")
+    loggig.info("Higgs 2 fraction: {higgs_2_frac:.4f}")
+    loggig.info("Higgs 3 fraction: {higgs_3_frac:.4f}")
+    loggig.info("All Higgs fraction: {all_higgs_frac:.4f}")
+
+
+if __name__ == "__main__":
+    main()
