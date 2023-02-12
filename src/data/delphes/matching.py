6279d9233f7af5ae240bc973c0961f270f23b8c1
Billy Li <li000400@umn.edu>
2023-02-12 14:38:16 -0800

    dev

--- src/data/delphes/matching.py
+++ src/data/delphes/matching.py
@@ -1,6 +1,7 @@
 import awkward as ak
 import numba as nb
 import vector
+import numpy as np
 
 vector.register_awkward()
 vector.register_numba()
@@ -84,16 +85,10 @@ def match_higgs_to_jet(higgses, bquarks, jets, builder):
     for higgses_event, bquarks_event, jets_event in zip(higgses, bquarks, jets):
         builder.begin_list()
         for i, (jet, jet_flv) in enumerate(zip(jets_event, jets_event.flavor)):
-            # The line below doesn't have real functions 
-            # Jets were checked as b jets in convert_to_h5 59-65
-            # This comment is to remind that if we only apply b_matching
-            # at here the shape of pt, eta, phi, etc. will be inconsistent
-            if (jet_flv != 5) and (jet_flv != -5):
-                continue
-
+            match_idx = -1
             for j, (_, higgs_idx) in enumerate(zip(higgses_event, higgses_event.idx)):
                 for bquark, bquark_m1 in zip(bquarks_event, bquarks_event.m1):
-                    if bquark_m1 == higgs_idx and jet.deltaR(bquark) < JET_DR:
+                    if bquark_m1 == higgs_idx and jet.deltaR(bquark) < JET_DR and np.abs(jet_flv) == 5:
                         match_idx = j + 1  # index higgs as 1, 2, 3
             builder.append(match_idx)
         builder.end_list()
