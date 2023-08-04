a82429a414d68801f3616ccf248e03d0f39e06b1
Billy Li <li000400@umn.edu>
2023-08-03 17:12:42 -0700

    debug

--- src/models/test_baseline2.py
+++ src/models/test_baseline2.py
@@ -115,8 +115,7 @@ def main(test_file, event_file):
     #     }
     # )
 
-    num_events = ak.count(fj_pt, axis=-1)[0]
-    print(num_events)
+    num_events = len(fj_pt) 
     bh1_b_pred = np.ones(shape=(num_events, 1), dtype=int)
     bh2_b_pred = np.ones(shape=(num_events, 1), dtype=int)*2
     bh3_b_pred = np.ones(shape=(num_events, 1), dtype=int)*3
